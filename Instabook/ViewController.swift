57ca0487191c6099c52429ed482313f3de97384d
Kaleb Wijaya <wijayakaleb@gmail.com>
2019-04-25 15:38:22 +0700

    Initial Commit

---
+++ Instabook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instabook
+//
+//  Created by Kaleb Wijaya on 25/04/19.
+//  Copyright © 2019 Kaleb Wijaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
