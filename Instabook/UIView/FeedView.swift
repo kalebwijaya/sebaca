8cf0b7afd0ad9832debf0ff5c6cf8aa4722ce4e7
Raiman <wijayakaleb@gmail.com>
2019-04-26 18:47:03 +0700

    Feed Without Comment

--- Instabook/UIView/FeedView.swift
+++ Instabook/UIView/FeedView.swift
@@ -12,7 +12,7 @@ class FeedView: UIView {
 
     var line = UIBezierPath()
     
-    func graph(){
+    func profileLine(){
         line.move(to: .init(x:20, y:80))
         line.addLine(to: .init(x:bounds.width-20, y:80))
         #colorLiteral(red: 0.9567790627, green: 0.9569165111, blue: 0.9567491412, alpha: 1).setStroke()
@@ -20,8 +20,17 @@ class FeedView: UIView {
         line.lineWidth = 2
     }
     
+    func postLine(){
+        line.move(to: .init(x:0, y:445))
+        line.addLine(to: .init(x:bounds.width, y:445))
+        #colorLiteral(red: 0.8383060694, green: 0.8433454037, blue: 0.8431125283, alpha: 1).setStroke()
+        line.stroke()
+        line.lineWidth = 2
+    }
+    
     override func draw (_ rect: CGRect){
-        graph()
+        profileLine()
+        postLine()
     }
 
 }
